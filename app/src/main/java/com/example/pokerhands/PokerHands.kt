45e52b7031011121e360fea008d3a39d1ca3264c
khebrolu <khebrolu@us.ibm.com>
2020-01-30 15:38:08 -0500

    adding api for coding dojo

--- app/src/main/java/com/example/pokerhands/PokerHands.kt
+++ app/src/main/java/com/example/pokerhands/PokerHands.kt
@@ -2,4 +2,12 @@ package com.example.pokerhands
 
 class PokerHands {
 
+    /**
+     * @param playerA list of player A card values
+     * @param playerB list of player B card values
+     */
+    fun getWinner(playerA: List<Int>, playerB: List<Int>): String {
+
+        return "Invalid Input"
+    }
 }
--- app/src/test/java/com/example/pokerhands/PokerHandsTest.kt
+++ app/src/test/java/com/example/pokerhands/PokerHandsTest.kt
@@ -1,5 +1,38 @@
 package com.example.pokerhands
 
+import junit.framework.Assert.assertEquals
+import junitparams.JUnitParamsRunner
+import junitparams.Parameters
+import org.junit.Test
+import org.junit.runner.RunWith
+
+
+@RunWith(JUnitParamsRunner::class)
 class PokerHandsTest {
 
+    @Test
+    @Parameters(method = "getInput")
+    fun `test my input`(
+        message: String,
+        expected: String,
+        playerAValues: List<Int>,
+        playerBValues: List<Int>
+    ) {
+
+        val pokerHands = PokerHands()
+        val actualResult = pokerHands.getWinner(playerAValues, playerBValues)
+
+        assertEquals(message, expected, actualResult)
+    }
+
+    fun getInput() = arrayOf(
+        arrayOf(
+            "Less than 5 values for player A or Player B", "Invalid input",
+            listOf(2, 3, 4, 5), listOf(7, 8, 9, 10)
+        ),
+        arrayOf(
+            "High card with player B", "Player B wins",
+            listOf(2, 3, 4, 5, 6), listOf(7, 8, 9, 10, 11)
+        )
+    )
 }
